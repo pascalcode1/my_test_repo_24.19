2c52b5fd360b94d6d060fe3305adb04df47e1244
Vadim Timofeev <pascalcode1@gmail.com>
2024-10-30 14:45:23 +0500

    passwords test

---
+++ installation/2.sql
@@ -0,0 +1,5 @@
+declare
+begin
+    dbms_output.put_line('Raw password: [{password}]');
+    dbms_output.put_line('Password through get_decrypted_module_param_val: [{' || util_module.get_decrypted_module_param_val('password') || '}]');
+end;
