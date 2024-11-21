fdcc98a092f6fcd4e280df6ce593bdd5fc002709
Vadim Timofeev <pascalcode1@gmail.com>
2024-11-21 18:06:36 +0500

    removed util_module.get_decrypted_module_param_val

--- installation/2.sql
+++ installation/2.sql
@@ -1,5 +1,4 @@
 declare
 begin
     dbms_output.put_line('Raw password: [{password}]');
-    dbms_output.put_line('Password through get_decrypted_module_param_val: [{' || util_module.get_decrypted_module_param_val(:p_module_id, 'password') || '}]');
 end;
