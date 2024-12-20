ca2c27285473f0f94e7d1304e643b31db67b604c
Vadim Timofeev <pascalcode1@gmail.com>
2024-12-20 09:52:14 +0500

    1.4

---
+++ installation/1.1/10.sql
@@ -0,0 +1,4 @@
+declare
+begin
+    dbms_output.put_line('This sql is located in 1.4 tag in 1.1 folder');
+end;
--- installation/1.1/2.sql
+++
@@ -1,4 +0,0 @@
-declare
-begin
-    dbms_output.put_line('This sql is located in 1.3 tag in 1.1 folder');
-end;
